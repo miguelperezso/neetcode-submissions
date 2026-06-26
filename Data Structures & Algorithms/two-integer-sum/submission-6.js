03785bc002d16fef5b5772632831fe82b14823af
Miguel Pérez <miguelangelpsolis@gmail.com>
2026-06-25 18:16:20 -0600

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        //Map(1) { 3 => 0  }
+        const memory = new Map();
+        for(let i = 0; i < nums.length; i++){
+            console.log("memory", memory)
+            if(i===0){memory.set(nums[i], i); continue;}
+            let diff = target - nums[i];
+            console.log(diff)
+            
+            if(memory.get(diff) != undefined){
+                return [memory.get(diff), i]
+            }
+            memory.set(nums[i], i)
+                // console.log("yey")
+                // return [memory.get(target - nums[i]), i]
+            
+        }
+    }
+}
